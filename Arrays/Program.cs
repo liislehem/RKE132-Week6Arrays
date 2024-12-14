2e4b03d911840454e7f419b6052dece2bb7f55a1
liis lehemets <liis.lehemets@tktk.ee>
2024-12-14 14:19:38 +0200

    add week6 to code

---
+++ Arrays/Program.cs
@@ -0,0 +1,29 @@
+﻿
+//massiivid vajalikud,e t salvestada andmeid 1 kohas.
+//1 massiivis vaid 1 tüüpi andmed
+
+//string[] fruit = { "apples", "bananas", "oranges" };
+
+string[] fruit = new string[3];
+//deklareerides tuleb kindel olla mitu ja millised andmed lähevad sinna, hiljem ei saa muuta. 
+
+fruit[0] = "apples";
+fruit[1] = "bananas";
+fruit[2] = "oranges";
+Console.WriteLine($"{fruit[0]}, {fruit[1]}, {fruit[2]}");
+
+int fruitArrayLength = fruit.Length;
+
+Console.WriteLine($"There are {fruitArrayLength} in your array");
+
+for (int i = 0; i < fruitArrayLength; i++)
+{
+    fruit[i] = fruit[i].Replace(fruit[i][0], char.ToUpper(fruit[i][0]));
+}
+
+foreach (string element in fruit)
+{
+    Console.WriteLine(element);
+}
+//foreach ei võimalda muuta andmeid. Saab vaid kuvada või kasutada
+
---
+++ Week6StringsAndArrays/Program.cs
@@ -0,0 +1,48 @@
+﻿
+string hello = "we are the champions? ";
+
+int stringLenght = hello.Length;
+//string trimmedString = hello.Trim(); //sulud on siin nagu tegusõna, et tee midagi
+// teine variant on sama asi teha järgnevalt:
+hello = hello.Trim();
+
+char firstLetter = hello[0];
+Console.WriteLine(firstLetter);
+
+hello = hello.Replace(hello[0], char.ToUpper(hello[0])).Replace('?', '!');//siin asendan väikese h tähe suurega. 
+//kindlam on kasutada toUpper või lower sest sõnad võivad muutuda
+//replacei võib kasutada nii palju kui vaja
+
+firstLetter = hello[0];
+
+Console.WriteLine(firstLetter);
+
+Console.WriteLine(hello);
+
+
+
+//int wordCounter = 1;
+
+
+//for(int i = 0; i < hello.Length; i++)
+{
+
+   // if (char.IsWhiteSpace(hello[i]))
+    {
+      //  wordCounter++;
+    }
+   // if (hello[i] ==  ' ')
+    //{
+     //   wordCounter++;
+    //}
+
+    //Console.WriteLine($"{i} symbol in string: {hello[i]}");
+    //kommentaari pandud variant 1 sama asja jaoks
+}
+
+//Console.WriteLine($"Word count in {hello}: {wordCounter} words.");
+//Console.WriteLine(hello.Length); //esimese puhul peab seal olema trimString.Length
+//teise puhul on hello.Lenthg
+
+// tähe asukoht on alati length -1. Hello- tähti on 5, aga asukohad hakkavad alates 0st.
+
